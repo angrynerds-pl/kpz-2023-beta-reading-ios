7b847f74251440b304fa81bc2a51f29543fc5f74
Julia1204 <julia.gosciniak1@gmail.com>
2023-04-02 19:22:01 +0200

    initial commit

---
+++ BetaReading/BetaReadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetaReadingApp.swift
+//  BetaReading
+//
+//  Created by Julia Gościniak on 02/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetaReadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
